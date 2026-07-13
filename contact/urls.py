f80bf8f70c36a011e29ef8017219721d1b4955b3
LuizHendges <la.hendges.cardoso@gmail.com>
2026-07-13 08:23:07 -0300

    complete Agenda project

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,19 @@ from . import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.index, name='idenx'),
+    path('search/', views.search, name='search'),
+    path('', views.index, name='index'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # user
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
+
 ]
